a7157822df3f912f46bb9fb700a392d65327cea9
ARPIT-KANANI <arpitkanani4@gmail.com>
2022-08-18 00:28:53 -0400

    Tour Scroll and basics

---
+++ app/src/main/java/com/example/attendance_v10/BookTour.java
@@ -0,0 +1,17 @@
+package com.example.attendance_v10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class BookTour extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_tour);
+    }
+}
